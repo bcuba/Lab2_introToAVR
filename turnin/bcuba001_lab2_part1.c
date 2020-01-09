485d55b58b9a8304dbf0564d5e5fd4b045e0c2d7
Bennett Cuba <bcuba001@wch136-03.cs.ucr.edu>
2020-01-09 13:24:00 -0800

    part 2

--- turnin/bcuba001_lab2_part1.c
+++ turnin/bcuba001_lab2_part1.c
@@ -15,27 +15,21 @@
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
-	DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
 	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
 	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
-	unsigned char cntavail = 0x00;
-	
+
     /* Insert your solution below */
     while (1) {
 	tmpA = PINA;
-	if (tmpA == 0x00) { // all parking is open
-		cntavail = 0x04;
-	} else if(tmpA == 0x01 || tmpA == 0x02 || tmpA == 0x04 || tmpA == 0x08){ // 3 parking spots are open
-		cntavail = 0x03;	
-	} else if(tmpA == 0x03 || tmpA == 0x05 || tmpA == 0x06 || tmpA == 0x09 || tmpA == 0x0A || tmpA == 0x0C){ // 2 parkings are open
-		cntavail = 0x02;
-	} else if(tmpA == 0x07 || tmpA == 0x0B || tmpA == 0x0D || tmpA == 0x0D || tmpA == 0x0E){
-		cntavail = 0x01;
-	} else if(tmpA == 0x0F){
-		cntavail = 0x00;
-	}
-			
-	PORTC = cntavail;		
+	if (tmpA == 0x01) { // True if PA0 is 1 and PA1 is 0
+		tmpB = 0x01; // Sets tmpB to 0000001
+	} else {
+			tmpB = 0x00; // Sets tmpB to bbbbbb10
+							 // (clear rightmost 2 bits, then set to 10)
+	}	
+							 //
+	PORTB = tmpB;		
     }
     return 0;
 }
